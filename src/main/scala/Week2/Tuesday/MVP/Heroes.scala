f1ce6097d7af7ef144dbb6f0aa8c992b25a87579
Ashley George <ashgeo1986@gmail.com>
2025-02-11 17:28:57 +0000

    Extension

---
+++ src/main/scala/Week2/Tuesday/MVP/Heroes.scala
@@ -0,0 +1,17 @@
+package Week2.Tuesday.MVP
+
+object heroclass extends App {
+
+  case class Heroes(superName: String, power: String, age: Int, isMutant: Boolean)
+
+  val scarletWitch = Heroes.apply("Scarlet Witch", "Telekinesis", 30, true)
+  val thor = Heroes.apply("Thor", "Lightning", 1500, false)
+  val cyclops = Heroes.apply("Cyclops", "Laser Eyes", 35, true)
+
+  val copiedScarletWitch = scarletWitch.copy(power = "Mind Control")
+  val copiedThor = thor.copy(age: 1501)
+  val copiedCyclops = cyclops.copy(isMutant = false)
+
+
+}
+
